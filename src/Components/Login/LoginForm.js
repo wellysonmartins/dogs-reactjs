6efa068d5435b2368af5e2f4514a20f37682e115
Wellyson Martins <wellyson.tavares@elocc.ag>
2021-01-05 10:11:18 -0300

    add UserContet Component

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { TOKEN_POST, USER_GET } from "../../api";
+import { UserContext } from "../../UserContext";
 import Button from "../Form/Button";
 import Input from "../Form/Input";
 import useForm from "../Hooks/useForm";
@@ -9,33 +9,13 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  React.useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    if (token) {
-      getUser(token);
-    }
-  }, []);
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
+  const { userLogin } = React.useContext(UserContext);
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-      window.localStorage.setItem("token", json.token);
-      getUser(json.token);
+      userLogin(username.value, password.value);
     }
   }
 
