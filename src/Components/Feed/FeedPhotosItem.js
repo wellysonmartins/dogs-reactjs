766d8485e203d740c9a8cf45ad70ef3e4b6a02f3
Wellyson Martins <wellyson.tavares@elocc.ag>
2021-03-07 16:07:23 -0300

    Add Image component

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import styles from './FeedPhotosItem.module.css';
+import React from "react";
+import Image from "../Helper/Image";
+import styles from "./FeedPhotosItem.module.css";
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   function handleClick() {
@@ -7,7 +8,7 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   }
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );
